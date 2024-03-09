38fe13e07d8c2a6b5ee7abe47879d559225acc8b
Nahyun Kim <hedghog1004@naver.com>
2024-03-10 01:12:44 +0900

    ✨ Feat: FeedWidget 구현
    Related to: #3

--- Instagram/lib/src/widgets/image_data.dart
+++ Instagram/lib/src/widgets/image_data.dart
@@ -32,6 +32,7 @@ class IconsPath {
 
   static String get myProfileUrl => 'https://cdn.discordapp.com/attachments/1124558739211558993/1215771754308571146/autumncocoa.jpg?ex=65fdf6ae&is=65eb81ae&hm=e9177ec52036f9a2e405d0bb2583d31486f6a621a451feac4a424a9ad5a6584d&';
   static String get otherProfileUrl => 'https://github.com/nahy-512/nahy-512/assets/101113025/3fb8e968-e482-4aff-9334-60c41014a80f';
+  static String get mongiProfileUrl => 'https://github.com/nahy-512/nahy-512/assets/101113025/f16fcb86-dba3-46ca-9bf4-b6475db9305b';
   // Bottom Nav Icon
   static String get homeOff => '$basePath/ic_home_unselected.svg';
   static String get homeOn => '$basePath/ic_home_selected.svg';
@@ -48,4 +49,9 @@ class IconsPath {
   static String get alert => '$basePath/ic_heart.svg';
   static String get dm => '$basePath/ic_share.svg';
   static String get addStory => '$basePath/ic_add_story.svg';
+  static String get more => '$basePath/ic_more.svg';
+  static String get heartOff => '$basePath/ic_heart.svg';
+  static String get chat => '$basePath/ic_chat.svg';
+  static String get share => '$basePath/ic_share.svg';
+  static String get save => '$basePath/ic_bookmark.svg';
 }
